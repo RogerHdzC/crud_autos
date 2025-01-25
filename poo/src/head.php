b4872c1d4d6ebb5039b93dacbae8d135de9854f2
RogerHdzC <hdc.rogelio@gmail.com>
2025-01-25 01:05:00 -0600

    feat(poo): add POO version for marca

---
+++ poo/src/head.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>CRUD Autos <?php if($action): echo ucfirst($action); ?> <?php echo ucfirst($type); endif ?> </title>
+<link rel="stylesheet" href="css/main.css">
