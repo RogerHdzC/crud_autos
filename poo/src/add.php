b00d9072a283d7d2636dbb763b107db969d49251
RogerHdzC <hdc.rogelio@gmail.com>
2025-01-29 19:56:08 -0600

    feat(poo): add crud of modelos

--- poo/src/add.php
+++ poo/src/add.php
@@ -1,16 +1,25 @@
 <?php
 require_once 'classes/Marca.php';
+require_once 'classes/Modelo.php';
 $action = 'Agregar';
 include_once 'head.php';
 $type = $_GET['type'] ?? '';
 
+$marca = new Marca();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($type === 'marca') {
-        $marca = new Marca();
         $marca->add($_POST['marca_name']);
         header("Location: index.php");
         exit;
+    } elseif($type === 'modelo'){
+        $modelo = new Modelo();
+        $modelo->add($_POST['marca_id'], $_POST['modelo_name']);
+        header("Location: index.php");
+        exit;
     }
+}
+if($type === 'modelo'){
+    $marcas = $marca->getAll();
 }
 ?>
 
@@ -19,6 +28,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <?php if ($type === 'marca'): ?>
             <label>Nombre de la Marca:</label>
             <input type="text" name="marca_name" required>
+        <?php elseif ($type === 'modelo'): ?>
+            <label>Nombre del Modelo:</label>
+            <input type="text" name="modelo_name" required>
+            <label>Marca:</label>
+            <select name="marca_id">
+                <?php
+                foreach ($marcas as $marca) {
+                    echo "<option value='{$marca['marca_id']}'>{$marca['marca_name']}</option>";
+                }
+                ?>
+            </select>
         <?php endif; ?>
         <button type="submit">Guardar</button>
     </form>
--- poo/src/delete.php
+++ poo/src/delete.php
@@ -1,13 +1,19 @@
 <?php
 require_once 'classes/Marca.php';
+require_once 'classes/Modelo.php';
 $type = $_GET['type'] ?? '';
 $id = $_GET['id'] ?? '';
 
 if ($type && $id) {
-    if($type == "marca"){
+    if($type === "marca"){
         $marca = new Marca();
         $marca->delete($id);
         header("Location: index.php");
         exit;
-    }   
+    } elseif($type === "modelo"){
+        $modelo = new Modelo();
+        $modelo->delete($id);
+        header("Location: index.php");
+        exit;
+    } 
 }
